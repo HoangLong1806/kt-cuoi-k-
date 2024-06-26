492eb5ec1fbbdd7bc15bb93af93b72d1f3e079ff
HoangLong1806 <danghoanglong180602@gmail.com>
2024-06-26 10:09:28 +0700

    update navigation

---
+++ src/customer/components/HomeCarosel/MainCaroselData.js
@@ -0,0 +1,22 @@
+export const MainCaroselData = [
+  {
+    image: "https://luvinus.com/wp-content/uploads/2023/02/ve-thoi-trang-nam-2.png",
+    path: "/women/clothing/lengha_choli",
+  },
+  {
+    image: "https://via.placeholder.com/600/771796",
+    path: "women/clothing/saree",
+  },
+  {
+    image: "https://via.placeholder.com/600/24f355",
+    path: "women/clothing/salwar_kameez",
+  },
+  {
+    image: "https://via.placeholder.com/600/d32776",
+    path: "women/clothing/kurti",
+  },
+  {
+    image: "https://via.placeholder.com/600/f66b97",
+    path: "women/clothing/lehenga",
+  },
+];
