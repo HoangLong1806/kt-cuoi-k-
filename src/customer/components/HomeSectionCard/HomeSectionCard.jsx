6e4d76a75502c02fad4b3b085bd8c4ebf1fcebd4
HoangLong1806 <danghoanglong180602@gmail.com>
2024-06-26 15:59:50 +0700

    update slide data

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const HomeSectionCard = () => {
   return (
-    <div className='cursor-pointer flex flex col item'>
-      
+    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
+      <div className='h-[13rem] w-[10rem]'>
+        <img src='https://via.placeholder.com/150' alt='product' className=' object-cover object-top'   />
+      </div>
     </div>
   )
 }
